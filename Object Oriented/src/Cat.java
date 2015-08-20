f345127e4c673d3968df53cb2facacadca1d8d08
Cloveh <jtlovescars@gmail.com>
2015-08-20 16:53:26 -0700

    Lods of estuf

---
+++ Object Oriented/src/Cat.java
@@ -0,0 +1,15 @@
+
+public class Cat {
+
+	public String name;
+	public int age;
+	public String color;
+	public Cat(String name, int age, String color){
+		this.name = name;
+		this.age =age;
+		this.color = color;
+		
+		
+	}
+	
+}
---
+++ Object Oriented/src/ObjectOriented.java
@@ -0,0 +1,11 @@
+public class ObjectOriented {
+
+	public static void main(String[] args) {
+
+		Cat c = new Cat("Clover", 4, "Brown");
+		
+		Cat c2 = new Cat("Trixie", 3, "Orange");
+		
+	}
+
+}
